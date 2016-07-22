183743b8e15e2910e43f45d4da5716a013d37505
Christina Lewis Halpern <clh@allstarcode.org>
2016-07-22 14:49:37 -0400

    adding early obamafication verson
    by christina and navid

--- obamafication_christina.py
+++ obamafication_christina.py
@@ -1,23 +1,33 @@
-#copied from Alley
+#by Navid and Christina
 from Myro import *
 pic = makePicture("christina_pic.jpg")
-for p in getPixels(pic):
-   #newC = makeColor(getGreen(p),getRed(p),getBlue(p))
-   red = makeColor(217,26,33)
-   blue = makeColor(112,150,158)
-   yellow = makeColor(252,227,166)
-   dblue = makeColor(0,51,76)
-   if getGray(p) > 180: 
-       setColor(p,yellow)
-   elif getGray(p) > 120: 
-       setColor(p,blue)
-   elif getGray(p) > 60: 
-       setColor(p,red)
-   elif getGray(p) <= 60: 
-       setColor(p,dblue)
-show(pic)
-## show (pic)
-## pix = getPixels(pic)
-## gray = getGray(pix)
-## myList=[pix]
-## print (pix)
+show (pic)
+pix = getPixels(pic)
+gray = getGray(pix)
+ourpixels=[pix]
+print (pix)
+
+
+
+## You'll need to use the following functions:
+## getPixels(PICTURE)
+## getPixels returns a list of individual pixels in the picture you pass. So if you loop through the pixels list, you'll have a pixel you can modify! 
+## getRed(PIXEL)
+## Returns the red value of a pixel
+## getGreen(PIXEL)
+## Returns the green value of a pixel
+## getBlue(PIXEL)
+## Returns the blue value of a pixel
+## makeColor(RED,GREEN,BLUE)
+## Returns a color to make a pixel from
+## setColor(PIXEL, COLOR)
+## Sets a pixel to a specific color 
+## getGray(PIXEL)
+## Gets the gray value of a pixel
+## 
+## 
+## The Obamafication process works as follows: If a pixel's gray value is greater than 180, then change that pixel's color to Obama-Yellow.  If the gray value is greater than 120, then the pixel should be changed to Obama-Blue.  If the gray value is greater than 60, then the pixel should be changed to Obama-Red. Otherwise the pixel should be Obama-DarkBlue.
+## Obama-DarkBlue = makeColor(0,51,76)
+## Obama-Red = makeColor(217, 26, 33)
+## Obama-Blue = makeColor(112,150,158)
+## Obama-Yellow = makeColor(252, 227, 166)
